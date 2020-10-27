3d60b996f0ef27a433e74f821ab487839f7c852e
Heidi Patja <hpatja@gmail.com>
2020-10-27 11:44:41 +0100

    Ooops, forgot alt tag on image

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <main>
             <h1>UniCoin Trading</h1>
-            <img className="logo" src={logo} />
+            <img className="logo" alt={"UniCoin Trading logo"} src={logo} />
             <p>With UniCoin Trading there are no losers, only winners.</p>
             <p>Use our app to trade with top currencies such as UniCoin, BaCoin or IceCreamCoin.</p>
             <p>Make your life better - become a member today. Up we go!</p>
